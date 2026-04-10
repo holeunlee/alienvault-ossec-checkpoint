1512edb47d75edaff3a3056a21b3f6335b9fa5e1
holeunlee <holeun.lee@gmail.com>
2026-04-10 15:28:12 +0000

    move: opsgenie_alert.sh to scripts-and-stuff

--- scripts/opsgenie_alert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-/usr/bin/curl -XPOST 'https://api.opsgenie.com/v1/json/alert' -d '
-{
-     "apiKey": "[MASKED]",
-     "message" : "alert '$1' '$2' '$3'",
-     "teams" : ["[MASKED]"]
-}'
-
